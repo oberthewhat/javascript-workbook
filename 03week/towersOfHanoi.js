51012a06e354586992dd930d23f9f729ddb2fb37
John Obertubbesing <oberthewhat@gmail.com>
2019-08-30 22:47:26 -0500

    TOWERS IS HARD...but i got 4 passing =)

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,7 +13,12 @@ let stacks = {
   c: []
 };
 
-let userStart = startStack.makelowercase()
+//console.log for my brain while playing the game. Helps space things out on the console. 
+
+console.log('===========================================================================')
+console.log('hello! To play, choose row "a" and place peice onto row, "b" or "c". Then move around the numbers until you create a stack on the other rows. You can not place a larger number on top of a smaller number. Good luck!')
+console.log('===========================================================================')
+
 
 function printStacks() {
   console.log("a: " + stacks.a);
@@ -21,34 +26,91 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
-  userStart.pop()
-  endStack.splice()
+
+  let popped = stacks[startStack].pop()
+  stacks[endStack].push(popped)
+
+}
+function isLegal(startStack, endStack){
+    let endArray = stacks[endStack];
+  let startArray = stacks[startStack];
+  if (endArray[endArray.length -1] > startArray[startArray.length -1] || endArray.length == 0){
+    return true
+  } else {
+    console.log('A larger number can not be on top of a smaller number')
+    return false;
+  }
 
 }
 
-function isLegal() {
+function isValid(startStack){
   // Your code here
-  let legal = ['a','b','c']
-  for (i=0; i < legal.length; i++){
-  if(userStart !== [i]){
-    console.log('bad');
-    return false;
-  }};
 
+  // startStack = startStack.toLowerCase()
+  
+  let legal = ['a','b','c','A','B','C']
+  for (let i=0; i < legal.length; i++){
+    if(startStack == legal[i]){
+    return true;
+  } 
+  
+};
+console.log('Please enter only A, B or C')
+return false;
 }
 
 function checkForWin() {
   // Your code here
+let winner = [4, 3, 2, 1];
+let winC = true;
+let winB = true;
+if(stacks.c.length == 4){
+  for(let i = 0; i < stacks.c.length; i++){
+    console.log('c', winner[i], stacks.c[i])
+    if(winner[i] !== stacks.c[i]){
+      winC = false;
+
+    }}
+  } else {
+    winC = false
+  }
+
+  if(stacks.b.length == 4){
+    for(let i = 0; i < stacks.b.length; i++){
+      console.log('b', winner[i], stacks.b[i])
+      if(winner[i] !== stacks.b[i]){
+        winB = false;
+  
+      }}
+    } else {
+      winB = false
+    }
+
+//this is where a counter function could be called
+console.log("---------------")
+console.count(' Turn: ')
+console.log("---------------")
+if(winB || winC){
+console.log('Winner!')
+return true
+} else {
+  return false
+};
+return winB || winC;
+
 
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
+  if (isValid(startStack) && isValid(endStack) && isLegal(startStack,endStack)){
 
-
+    movePiece(startStack,endStack);
+    checkForWin()
+  } 
 }
+
 
 function getPrompt() {
   printStacks();
@@ -59,6 +121,8 @@ function getPrompt() {
     });
   });
 }
+
+
 
 // Tests
 
