84e54c8a1b814c5ea7427833d828beb05c980337
John Obertubbesing <oberthewhat@gmail.com>
2019-09-23 16:13:29 -0500

    chickacheckers

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,13 +8,17 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
-  // Your code here
+function Checkers() {
+  
 }
+
 
 class Board {
   constructor() {
     this.grid = []
+    this.redPiece = 'R',
+    this.blackPiece = 'B',
+    this.checkers = []
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -38,7 +42,7 @@ class Board {
         // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
           // push the symbol of the check in that location into the array
-          rowOfCheckers.push(this.grid[row][column].symbol);
+          rowOfCheckers.push(this.grid[row][column])
         } else {
           // just push in a blank space
           rowOfCheckers.push(' ');
@@ -50,19 +54,194 @@ class Board {
       string += "\n";
     }
     console.log(string);
+  } 
+
+
+
+  starterPieces() {
+
+
+    //laying out the red pieces
+  for(let row1 = 0; row1 < 3; row1++){
+    for(let col1 = 0; col1 < 8; col1++){
+        if(row1 % 2 == 0 && col1 %2 == 1){
+          this.grid[row1][col1] = this.redPiece
+          this.checkers.push(this.redPiece)
+        }
+     }
+   }
+   for(let row1 = 1; row1 < 2; row1++){
+    for(let col1 = 0; col1 < 8; col1++){
+        if(row1 % 2 == 1 && col1 %2 == 0){
+          this.grid[row1][col1] = this.redPiece
+          this.checkers.push(this.redPiece)
+        }
+     }
+   }
+
+   //laying out black pieces
+   for(let row2 = 5; row2 < 8; row2++){
+    for(let col2 = 0; col2 < 8; col2++){
+        if(row2 % 2 == 1 && col2 %2 == 0){
+          this.grid[row2][col2] = this.blackPiece
+          this.checkers.push(this.blackPiece)
+        }
+     }
+   }
+   for(let row2 = 6; row2 < 7; row2++){
+    for(let col2 = 0; col2 < 8; col2++){
+        if(row2 % 2 == 0 && col2 %2 == 1){
+          this.grid[row2][col2] = this.blackPiece
+          this.checkers.push(this.blackPiece)
+        }
+     }
+   }
   }
 
-  // Your code here
+
 }
 
 class Game {
   constructor() {
     this.board = new Board;
+    this.player = "B" 
   }
   start() {
     this.board.createGrid();
+    this.board.starterPieces();
+  }
+ moveChecker(whichPiece, toWhere){
+
+    var whichPieceArr = whichPiece.split('');
+    var toWhereArr = toWhere.split('');
+    var chosen = this.board.grid[whichPieceArr[0]][whichPieceArr[1]]
+
+
+  if(chosen === this.player && this.validInput(whichPiece, toWhere) && this.validMove(whichPiece,toWhere) || this.jumpMove(whichPiece, toWhere)){
+   this.board.grid[toWhereArr[0]][toWhereArr[1]] = chosen
+   this.board.grid[whichPieceArr[0]][whichPieceArr[1]] = null;
+   this.playerTurn()
+} else {
+
+}
+ }
+validInput(whichPiece, toWhere){
+  var whichPieceArr = whichPiece.split('');
+  var toWhereArr = toWhere.split('');
+  if(whichPiece.length !== 2 || whichPieceArr[0] > 7 || whichPieceArr[1] > 7){
+    console.log('Please enter numbers less than 8')
+    return false;
+  }
+  if(toWhere.length !== 2 || toWhereArr[0] > 7 || toWhereArr[1] > 7){
+    console.log('Please enter numbers less than 8')
+    return false;
+  }
+  return true;
+
+}
+jumpMove(whichPiece,toWhere){
+  var whichPieceArr = whichPiece.split('');
+  var toWhereArr = toWhere.split('');
+  var chosen = this.board.grid[whichPieceArr[0]][whichPieceArr[1]];
+  var dropSpot = this.board.grid[toWhereArr[0]][toWhereArr[1]];
+  var redJumpRight = this.board.grid[Number(whichPieceArr[0]) +1][Number(whichPieceArr[1]) +1]
+  var redJumpLeft = this.board.grid[Number(whichPieceArr[0]) +1][Number(whichPieceArr[1]) -1]
+  var blackJumpRight = this.board.grid[Number(whichPieceArr[0]) -1][Number(whichPieceArr[1]) +1]
+  var blackJumpLeft = this.board.grid[Number(whichPieceArr[0]) -1][Number(whichPieceArr[1]) -1]
+
+
+  if(chosen == null){
+    return false;
+    }
+    if(dropSpot !== null){
+      return false;
+    
+    }
+    
+    if(chosen == 'B'){
+      if(whichPiece - toWhere !== 18 && whichPiece - toWhere !== 22)
+    {
+        return false
+      } else {
+        if(blackJumpRight === 'R' && whichPiece - toWhere === 18){
+          this.board.grid[Number(whichPieceArr[0]) -1][Number(whichPieceArr[1]) +1] = null
+          return true;
+        } 
+        if(blackJumpLeft === 'R' && whichPiece - toWhere === 22){
+          blackJumpLeft = null
+          return true;
+        }
+        else { 
+          console.log('You can not jump over an empty space or your own piece')
+          return false;
+        } 
+        
+      }
+    }
+
+
+
+    if(chosen == 'R'){
+      if(toWhere - whichPiece !== 18 && toWhere - whichPiece !== 22){
+        return false
+      } else {
+        if(redJumpRight === 'B' && whichPiece - toWhere === 22){
+          redJumpRight = null
+        }
+        if(redJumpLeft === 'B' && whichPiece - toWhere === 18){
+          redJumpLeft = null
+        }         
+        else { 
+          console.log('You can not jump over an empty space or your own piece')
+        } 
+
+    }
+    
+    }
+}
+validMove(whichPiece, toWhere){
+  var whichPieceArr = whichPiece.split('');
+  var toWhereArr = toWhere.split('');
+  var chosen = this.board.grid[whichPieceArr[0]][whichPieceArr[1]];
+  var dropSpot = this.board.grid[toWhereArr[0]][toWhereArr[1]];
+
+  
+if(chosen == null){
+return false;
+}
+if(dropSpot !== null){
+  return false;
+
+}
+
+if(chosen == 'B'){
+  if(whichPiece - toWhere !== 9 && whichPiece - toWhere !== 11)
+  {
+    return false
   }
 }
+if(chosen == 'R'){
+  if(toWhere - whichPiece !== 9 && toWhere - whichPiece !== 11){
+    return false
+  }
+}
+return true
+
+}
+
+playerTurn(){
+  if(this.player === 'B'){
+    this.player = 'R'
+    console.log('#######  Red player turn  #######')
+  } else{
+    this.player = 'B'
+    console.log('#######  Black player turn  #######')
+  }
+}
+
+}
+
+
 
 function getPrompt() {
   game.board.viewGrid();
@@ -74,11 +253,13 @@ function getPrompt() {
   });
 }
 
+
 const game = new Game();
 game.start();
 
-
-// Tests
+//*********************************************************************************************************
+// TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
+//*********************************************************************************************************
 if (typeof describe === 'function') {
   describe('Game', () => {
     it('should have a board', () => {
