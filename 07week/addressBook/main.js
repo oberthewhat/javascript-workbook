dbcae993414ddacb33c350efabdc625b63280ea2
John Obertubbesing <oberthewhat@gmail.com>
2019-09-30 23:05:43 -0500

    whoops

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -63,16 +63,3 @@ const details = (result) => {
 
 
 
-
-// const listPeopleChoices = () => {
-//   const listElement = document.getElementById('people')
-//   arrOfPeople.map(person => {
-//     const li = document.createElement("li")
-//     const button = document.createElement("button")
-//     button.innerHTML = "Make Player"
-//     button.addEventListener('click', function() {makePlayer(person.id)} )
-//     li.appendChild(button)
-//     li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
-//     listElement.append(li)
-//   })
-// }
