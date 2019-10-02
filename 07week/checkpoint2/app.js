879fff2cbb72a7dd6203d10e20da44b60ba21491
John Obertubbesing <oberthewhat@gmail.com>
2019-10-02 16:47:51 -0500

    updated, still issue with removing player from team

--- 07week/checkpoint2/app.js
+++ 07week/checkpoint2/app.js
@@ -11,7 +11,7 @@ const arrOfPeople = [
   {
     id: 3,
     name: "Andrew Judy",
-    age: 35,
+    age: 32,
     skillSet: "Offsides official",
     placeBorn: "Louisville, Kentucky"
   },
@@ -33,21 +33,21 @@ const arrOfPeople = [
     id: 6,
     name: "Stan Honest",
     age: 20,
-    skillSet: "boom-a-rang throwing",
+    skillSet: "As honest as they come",
     placeBorn: "Perth, Australia"
   },
   {
     id: 7,
-    name: "Mia Watu",
+    name: "Adam Darrenkamp",
     age: 17,
-    skillSet: "acrobatics",
+    skillSet: "looking good",
     placeBorn: "Los Angeles, California"
   },
   {
     id: 8,
-    name: "Walter Cole",
+    name: "shimlock Kelter",
     age: 32,
-    skillSet: "jump rope",
+    skillSet: "Makes a mean bowl of cereal",
     placeBorn: "New Orleans, Louisiana"
   },
 ]
@@ -74,59 +74,59 @@ class redTeammate extends player {
   }
 }
 
-const playerArrCheck = () => {
-  for(let i=0;i<listOfPlayers.length;i++){
-    console.log(listOfPlayers[i])
-  }
-}
-
+//dispalys the list of players when the button is clicked, also creates a button for the user to move the person into the players array. 
 const listPeopleChoices = () => {
   const remButt = document.getElementById('listButton')
- remButt.style.display = 'none'
+  remButt.style.display = 'none' //removes the button to move display more players
   const listElement = document.getElementById('people')
-  listElement.innerHTML = ''
+  listElement.innerHTML = ''//removes player from the list after selected
   arrOfPeople.map(person => {
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML = "Make Player"
+
+    //adds the button function for making a player, passes the person at the id into the make player function. splices the person from the array of people.
     button.addEventListener('click', function() {
       makePlayer(person.id)
       arrOfPeople.splice(arrOfPeople.indexOf(person), 1)
       listPeopleChoices()
       
     })
+    //appends the button and the person name and skill to the list on the DOM
     li.appendChild(button)
     li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
     listElement.append(li)
   })
 }
 
+//the function that is called when a user presses make player.
 const makePlayer = (id) => {
   const playersSection = document.getElementById('players')
+  //maps through the array of people and compares it to what was selected from the list of people choices. Pushes that person to the list of players array
   arrOfPeople.map(person => { 
     if(id === person.id){
     listOfPlayers.push(person)
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML = "Remove"
+    //makes a button to remove the plater from the player list and put back into the array of people.
     button.addEventListener('click', function() { 
       const popped = listOfPlayers.splice(listOfPlayers.indexOf(person),1)
       arrOfPeople.push(popped[0])
-      
       listPeopleChoices()
       playersSection.removeChild(li)
 
     })
-    console.log('this shows the array of people' , arrOfPeople)
-    console.log('this shows the array of players' , listOfPlayers)
+    //create a blue button for users to chose which team.
     const blueButt = document.createElement("button")
     blueButt.innerHTML = "B";
     blueButt.style.backgroundColor='#1E35F7'
+    //adds a function for the button when clicked to move the player to the blue team and remove from the player list.
     blueButt.addEventListener('click', function() {
-      console.log('blue clicked')
       makeBlue()
       playersSection.removeChild(li)
     })
+    //create a red button for users to send player to red team.
     const redButt = document.createElement("button")
     redButt.innerHTML = "R"
     redButt.style.backgroundColor = "#F71E35"
@@ -136,7 +136,7 @@ const makePlayer = (id) => {
       listOfPlayers.splice(person[0])
       playersSection.removeChild(li)
     })
-    
+    //appends the buttons and player info into the DOM
     li.appendChild(button)
     li.appendChild(blueButt)
     li.appendChild(redButt)
@@ -145,19 +145,20 @@ const makePlayer = (id) => {
   }})}
   
 const makeRed = () => {
-  const playersSection = document.getElementById('players')
+  document.getElementById('players')
   const redList = document.getElementById('red')
   
   listOfPlayers.map(person => { 
     redTeam.push(person)
+    listOfPlayers.splice(listOfPlayers.indexOf(person), 1)
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML = "Remove"
     button.addEventListener('click', function() { 
-      const popped = redList.splice(redList.indexOf(person),1)
-      listOfPlayers.push(popped[0])
+      redTeam.splice(blueTeam.indexOf(person),1)
       redList.removeChild(li)
-
+      arrOfPeople.push(person)
+      makePlayer(person.id)
     })
 
     li.appendChild(button)
@@ -168,19 +169,21 @@ const makeRed = () => {
 
 
   const makeBlue = () => {
-    const playersSection = document.getElementById('players')
+    document.getElementById('players')
     const blueList = document.getElementById('blue')
     
     listOfPlayers.map(person => { 
-      redTeam.push(person)
+      blueTeam.push(person)
+      listOfPlayers.splice(listOfPlayers.indexOf(person), 1)
       const li = document.createElement("li")
       const button = document.createElement("button")
       button.innerHTML = "Remove"
       button.addEventListener('click', function() { 
-        const popped = redList.splice(redList.indexOf(person),1)
-        listOfPlayers.push(popped[0])
+        blueTeam.splice(blueTeam.indexOf(person),1)
         blueList.removeChild(li)
-  
+        arrOfPeople.push(person)
+        makePlayer(person.id)
+        console.log('remove from blue team button pressed ',listOfPlayers)
       })
   
       li.appendChild(button)
