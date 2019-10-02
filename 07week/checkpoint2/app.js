66af46db115a95c6b86d52de9e7814d681dd6cd0
John Obertubbesing <oberthewhat@gmail.com>
2019-10-01 23:06:10 -0500

    just a few bugs

--- 07week/checkpoint2/app.js
+++ 07week/checkpoint2/app.js
@@ -3,24 +3,24 @@
 const arrOfPeople = [
   {
     id: 2,
-    name: "Charles Young",
+    name: "Charles McDerkinstan",
     age: 55,
-    skillSet: "welding",
+    skillSet: "Duck Breeder",
     placeBorn: "Omaha, Nebraska"
   },
   {
     id: 3,
-    name: "Judy Twilight",
+    name: "Andrew Judy",
     age: 35,
-    skillSet: "fishing",
+    skillSet: "Offsides official",
     placeBorn: "Louisville, Kentucky"
   },
   {
     id: 4,
-    name: "Cynthia Doolittle",
+    name: "Millie Obertubbesing ",
     age: 20,
-    skillSet: "tic tac toe",
-    placeBorn: "Pawnee, Texas"
+    skillSet: "Collecting items in one hand",
+    placeBorn: "Austin, Texas"
   },
   {
     id: 5,
@@ -57,22 +57,44 @@ const blueTeam = []
 const redTeam = []
 
 class player {
-  constructor(){}
+  constructor(player){}
 }
-class blueTeammate {
-  constructor(){}
+
+class blueTeammate extends player {
+  constructor(){
+    this.color = 'blue'
+    super(player)
+  }
 }
-class redTeammate {
-  constructor(){}
+
+class redTeammate extends player {
+  constructor(){
+    this.color = 'red'
+    super(player)
+  }
+}
+
+const playerArrCheck = () => {
+  for(let i=0;i<listOfPlayers.length;i++){
+    console.log(listOfPlayers[i])
+  }
 }
 
 const listPeopleChoices = () => {
+  const remButt = document.getElementById('listButton')
+ remButt.style.display = 'none'
   const listElement = document.getElementById('people')
+  listElement.innerHTML = ''
   arrOfPeople.map(person => {
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML = "Make Player"
-    button.addEventListener('click', function() {makePlayer(person.id)} )
+    button.addEventListener('click', function() {
+      makePlayer(person.id)
+      arrOfPeople.splice(arrOfPeople.indexOf(person), 1)
+      listPeopleChoices()
+      
+    })
     li.appendChild(button)
     li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
     listElement.append(li)
@@ -81,18 +103,88 @@ const listPeopleChoices = () => {
 
 const makePlayer = (id) => {
   const playersSection = document.getElementById('players')
-  arrOfPeople.map(person => {
+  arrOfPeople.map(person => { 
     if(id === person.id){
+    listOfPlayers.push(person)
     const li = document.createElement("li")
     const button = document.createElement("button")
-    button.innerHTML = "Remove Player"
-    button.addEventListener('click', function() {
-      person.innerHTML = ''
-    })
-    button.addEventListener('click', function() {makePlayer(person.id)} )
-    playersSection.appendChild(button)
-    playersSection.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
-  }
-  })
+    button.innerHTML = "Remove"
+    button.addEventListener('click', function() { 
+      const popped = listOfPlayers.splice(listOfPlayers.indexOf(person),1)
+      arrOfPeople.push(popped[0])
+      
+      listPeopleChoices()
+      playersSection.removeChild(li)
 
-}
+    })
+    console.log('this shows the array of people' , arrOfPeople)
+    console.log('this shows the array of players' , listOfPlayers)
+    const blueButt = document.createElement("button")
+    blueButt.innerHTML = "B";
+    blueButt.style.backgroundColor='#1E35F7'
+    blueButt.addEventListener('click', function() {
+      console.log('blue clicked')
+      makeBlue()
+      playersSection.removeChild(li)
+    })
+    const redButt = document.createElement("button")
+    redButt.innerHTML = "R"
+    redButt.style.backgroundColor = "#F71E35"
+    redButt.addEventListener('click', function() {
+      console.log('red clicked')
+      makeRed()
+      listOfPlayers.splice(person[0])
+      playersSection.removeChild(li)
+    })
+    
+    li.appendChild(button)
+    li.appendChild(blueButt)
+    li.appendChild(redButt)
+    li.appendChild(document.createTextNode(" - " + person.name + " - " + person.skillSet))
+    playersSection.appendChild(li)
+  }})}
+  
+const makeRed = () => {
+  const playersSection = document.getElementById('players')
+  const redList = document.getElementById('red')
+  
+  listOfPlayers.map(person => { 
+    redTeam.push(person)
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+    button.innerHTML = "Remove"
+    button.addEventListener('click', function() { 
+      const popped = redList.splice(redList.indexOf(person),1)
+      listOfPlayers.push(popped[0])
+      redList.removeChild(li)
+
+    })
+
+    li.appendChild(button)
+    li.appendChild(document.createTextNode(" - " + person.name + " - " + person.skillSet))
+    redList.appendChild(li)
+  })}
+
+
+
+  const makeBlue = () => {
+    const playersSection = document.getElementById('players')
+    const blueList = document.getElementById('blue')
+    
+    listOfPlayers.map(person => { 
+      redTeam.push(person)
+      const li = document.createElement("li")
+      const button = document.createElement("button")
+      button.innerHTML = "Remove"
+      button.addEventListener('click', function() { 
+        const popped = redList.splice(redList.indexOf(person),1)
+        listOfPlayers.push(popped[0])
+        blueList.removeChild(li)
+  
+      })
+  
+      li.appendChild(button)
+      li.appendChild(document.createTextNode(" - " + person.name + " - " + person.skillSet))
+      blueList.appendChild(li)
+    })}
+  
